01a64f57d2de6ce8946c460de71b75ff48ed36ee
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2022-02-15 03:17:49 +0100

    added solution
    added solution (studio)

---
+++ cleanCode/cleanCode/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/// <summary>
+/// Программа"Угадай число"
+/// </summary>
+Console.WriteLine("Hello, World!");
+
+
+
+
+string name = "Сергей Сергеевич";
+Console.WriteLine(name);
+
